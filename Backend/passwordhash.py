c79e0dcad4acaabe6b7370d737b92fdc41dc99f9
boysanic <boysonicrevived@gmail.com>
2018-11-10 13:10:02 -0500

    Theoretically, that's password hashing done

--- Backend/passwordhash.py
+++ Backend/passwordhash.py
@@ -2,15 +2,21 @@
 import hashlib
 import secrets
 import string
-def sha256(message):
-	m = hashlib.sha256()
-	m.update(str.encode(message))
-	return m
+import binascii
+
+def pbkdf2_hmac(message, salt):
+	iterations = 1000000
+	hashed = hashlib.pbkdf2_hmac('sha512', str.encode(message), str.encode(salt), iterations)
+	return hashed
 def getSalt(size):
 	chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
 	return ''.join(secrets.choice(chars) for _ in range(size))
 
 pw = "test"
-salt = getSalt(14)
-saltedpw = pw + salt
-m = sha256(saltedpw)
+salt = getSalt(64)
+#saltedpw = pw + salt
+#m = sha256(saltedpw)
+#print(m.hexdigest())
+#print(salt)
+hashed = pbkdf2_hmac(pw, salt)
+print(binascii.hexlify(hashed))
