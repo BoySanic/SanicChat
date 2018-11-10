479c8008036829469e98a9033bc3228ed9bffe5f
boysanic <boysonicrevived@gmail.com>
2018-11-10 11:46:49 -0500

    Working server with a client

--- Backend/server.py
+++ Backend/server.py
@@ -1,3 +1,18 @@
 #! /usr/bin/python3
+import socket
 
 print("SanicChat Server starting...");
+TCP_IP = '127.0.0.1'
+TCP_PORT = 3001
+BUFFER_SIZE = 20
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))	
+s.listen(1)
+while 1:
+	conn, addr = s.accept()
+	print("Connection Address: %s", addr)
+	data = conn.recv(BUFFER_SIZE)
+	if not data: continue
+	print("Received data: ", data.decode())
+	conn.send(data) #echo
+	conn.close()
